167fce5e3f61543cc9af2a2ea7d8396148bbdc47
emilya <emilya.31.06@mail.ru>
2022-07-12 01:01:55 +0300

    первое задание

---
+++ Задание1/Program.cs
@@ -0,0 +1,12 @@
+﻿
+ int  num1, num2;
+Console.Write("Input first integer number : ");
+num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input second integer number : ");
+num2 = Convert.ToInt32(Console.ReadLine());
+ if ( num1> num2 )  
+ {
+    Console.WriteLine($"{num1}-max, {num2}-min" );
+ }
+ if ( num1 < num2 )  Console.WriteLine($"{num2}-max, {num1}-min");
+ if (num1 == num2 ) Console.WriteLine ("Введите разные числа!");
