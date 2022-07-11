7bd5c7cbb12a532962bcfa37c09ef0e3a1d5dade
emilya <emilya.31.06@mail.ru>
2022-07-12 01:01:14 +0300

    третье задание

---
+++ Задание1/Задание2/Задание3/Program.cs
@@ -0,0 +1,9 @@
+﻿
+int num;
+Console.Write("Input integer number : ") ;
+num = Convert.ToInt32(Console.ReadLine());
+if ( num  % 2 == 0 )
+{
+    Console.Write($" {num}-число четное");
+}
+else Console.WriteLine($" {num}-число нечетное!");
