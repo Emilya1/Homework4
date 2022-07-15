70c631e44804d8a51520584e8c6ff4494448cf28
emilya <emilya.31.06@mail.ru>
2022-07-15 09:42:37 +0300

    Новые изменения

---
+++ Задание1/HomeWork2/Program.cs
@@ -0,0 +1,56 @@
+﻿/*  
+// Задание 1
+
+int FindSecondDigit(int num)
+{
+    int number1 = num / 10;
+    int number2 = number1 % 10;
+    return number2;
+
+}
+Console.Write("Введите трехзначное число : ");
+int num = Convert.ToInt32(Console.ReadLine());
+int result = FindSecondDigit(num);
+Console.WriteLine(result);
+
+
+
+//Задание 2
+int FindThirdDigit(int num)
+{
+    if (num < 100)
+        Console.WriteLine(" Третьей цифры нет");
+        
+
+    else
+        while (num > 1000 && num > 99)
+        {
+            num = num / 10;
+        }
+    num = num % 10;
+    return num;
+
+}
+Console.Write("Введите число : ");
+int num = Convert.ToInt32(Console.ReadLine());
+int result = FindThirdDigit(num);
+Console.WriteLine(result);
+
+
+// Задание 3
+bool DayOff(int num)
+{
+    if (num == 6 || num == 7)
+        return true;
+
+    else return false;
+}
+Console.WriteLine("Введите число от 1 до 7 : ");
+int number = Convert.ToInt32(Console.ReadLine());
+bool DayWeek = DayOff(number);
+Console.WriteLine(DayWeek);
+
+*/
+
+
+
